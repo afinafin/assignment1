e4b720a8caa9c1c7a61f5ddd11cb4da11047f653
afinafin <88936452+afinafin@users.noreply.github.com>
2021-12-18 23:08:49 +0800

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,46 @@
+<?php
+$target_dir = getcwd()."/uploadedFile/";
+//$target_dir = "/uploadedFile/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$fileExt= strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$fileName = pathinfo($target_file,PATHINFO_FILENAME);
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is a " . $fileExt . " type.";
+    $uploadOk = 1;
+  } else {
+    echo "File is not a pdf.";
+    $uploadOk = 0;
+  }
+}
+
+// Allow certain file formats
+if($fileExt != "pdf") {
+  echo "\n\nSorry, pdf file are allowed";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "\n\nSorry, your file was not uploaded.";
+  
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "\n\nThe file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+
+        //record the file name in txt file
+        $txt = fopen(getcwd() . "/fileName.txt", "w");
+        fwrite($txt, $fileName);
+        
+
+    } else {
+        echo "\n\nSorry, there was an error uploading your file.";
+        echo "\n " . $_FILES["fileToUpload"]["error"];
+    }
+}
+?>
